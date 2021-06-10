c699666c660e57ee1766f478d5a3ad1f5f8abd34
betocostadev <robertomarx@gmail.com>
2021-06-10 19:38:04 -0300

    added schemas to order and order items

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -198,7 +198,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="1v73yg8"
+        adminMetaHash="9xx9h6"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
